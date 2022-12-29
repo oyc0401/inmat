e9eb4b769f8b0cd5727d13d0bdfced79b70d0e3d
oyuchan <oyc0401@gmail.com>
2022-12-29 23:20:18 +0900

    네이버 맵 추가하기

---
+++ lib/_sample/naver_map/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'naver.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '네이버 맵',
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/_sample/naver_map/naver.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This is used in the platform side to register the view.
+    const String viewType = '<platform-view-type>';
+    // Pass parameters to the platform side.
+    const Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("네이버 맵"),
+      ),
+      body: Column(children: [
+
+      ],),
+    );
+  }
+}
