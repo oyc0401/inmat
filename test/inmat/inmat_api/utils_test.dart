2f76f56ef946e8ed4970a62a093669bd0c0979cf
oyuchan <oyc0401@gmail.com>
2023-02-04 00:39:59 +0900

    s3 업로드

---
+++ test/inmat/inmat_api/utils_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:inmat/inmat/auth/domain/model/profile_model.dart';
+import 'package:inmat/inmat/auth/domain/model/token_model.dart';
+import 'package:inmat/inmat/auth/inmat_auth.dart';
+import 'package:inmat/inmat/inmat_api/inmat_api.dart';
+
+void main() {
+  group("account api 테스트", () {
+    late TokenModel testToken;
+
+    const String testId = "flutter1";
+    const String testPassword = "qwe12345&&";
+    const String testDeviceIdentifier = "dart_pad";
+
+    setUpAll(() async {
+      // 로그인
+      Map<String, dynamic> json = await InMatApi.auth.login(
+        id: testId,
+        password: testPassword,
+        deviceIdentifier: testDeviceIdentifier,
+      );
+
+      testToken = TokenModel.fromJson(json);
+    });
+
+    test("서명된 url 얻기", () async {
+      String url =
+          await InMatApi.utils.getImageUrl("image.jpg", testToken.accessToken);
+      print(url);
+    });
+  });
+}
